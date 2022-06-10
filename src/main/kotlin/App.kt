5d70c3f906cf5be2e96c4fde0f6dde8e4dc55037
tiramshu <shunflower320@gmail.com>
2022-06-10 16:28:56 +0800

    add video player component

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -14,19 +14,18 @@ import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.p
 import react.dom.html.ReactHTML.img
 
-val unwatchedVideos = listOf(
-    Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
-    Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
-    Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg")
-)
-
-val watchedVideos = listOf(
-    Video(4, "Creating Internal DSLs in Kotlin", "Venkat Subramaniam", "https://youtu.be/JzTeAM8N1-o")
-)
-
 val App = FC<Props> {
+    // Stateful stuff
     var currentVideo: Video? by useState(null)
-    // typesafe HTML goes here, starting with the first h1 tag!
+    var unwatchedVideos: List<Video> by useState(listOf(
+        Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
+        Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
+        Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg")
+    ))
+    var watchedVideos: List<Video> by useState(listOf(
+        Video(4, "Creating Internal DSLs in Kotlin", "Venkat Subramaniam", "https://youtu.be/JzTeAM8N1-o")
+    ))
+
     h1 {
         +"Hello, Hui Ren!"
     }
@@ -52,17 +51,20 @@ val App = FC<Props> {
             }
         }
     }
-    div {
-        css {
-            position = Position.absolute
-            top = 10.px
-            right = 100.px
-        }
-        h3 {
-            +"John Doe: Building and breaking things"
-        }
-        img {
-            src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+    // let: scope function, only add VideoPlayer comp when state.currentVideo is not null
+    currentVideo?.let {curr ->
+        VideoPlayer {
+            video = curr
+            unwatchedVideo = curr in unwatchedVideos
+            onWatchedButtonPressed = {
+                if (video in unwatchedVideos) {
+                    unwatchedVideos = unwatchedVideos - video
+                    watchedVideos = watchedVideos + video
+                } else {
+                    watchedVideos = watchedVideos - video
+                    unwatchedVideos = unwatchedVideos + video
+                }
+            }
         }
     }
 }

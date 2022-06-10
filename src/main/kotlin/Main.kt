b23ca23d9b1eaab287bac3a095f9949cf617491b
tiramshu <shunflower320@gmail.com>
2022-06-10 15:25:09 +0800

    page with kotlin dsl

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,52 @@
 import kotlinx.browser.document
+import react.*
+import react.css.css
+import react.dom.render
+import csstype.Position
+import csstype.px
+import react.dom.html.ReactHTML.h1
+import react.dom.html.ReactHTML.h3
+import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.p
+import react.dom.html.ReactHTML.img
+import kotlinx.serialization.Serializable
+
+val unwatchedVideos = listOf(
+    Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
+    Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
+    Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg")
+)
 
 fun main() {
-    document.bgColor = "blue"
+    document.bgColor = "green"
+    val container = document.getElementById("root") ?: error("Couldn't find root container!")
+
+    render(Fragment.create {
+        h1 {
+            +"Hello, Hui Ren!"
+        }
+        div {
+            h3 {
+                +"Videos to watch"
+            }
+            for (video in unwatchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
+            }
+        }
+        div {
+            css {
+                position = Position.absolute
+                top = 10.px
+                right = 100.px
+            }
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+            img {
+                src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+            }
+        }
+    }, container)
 }
